4a79644baecac91ef02ab3c0553f125987b64c9c
minhyuk <kazesky93@gmail.com>
2024-10-14 19:12:47 +0900

    1차 배포상태

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,56 +1,13 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'gh-blog',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+  router: {
+    base: '/gh-blog/'
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
-
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     '@nuxt/content'
   ],
-  ssr: false,
-  nitro : {
-    preset : 'github-pages'
-  },
-  app : {
-    baseURL : '/gh-blog/'
-    // baseURL : '/'
-  },
-  content: {
-    documentDriven: true
-  },
+  // Router 설정: GitHub Pages 서브 디렉토리 경로 설정
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+};
