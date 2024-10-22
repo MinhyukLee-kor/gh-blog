2121518b8602d8a1f5e485c87bae759e600405d4
minhyuk <kazesky93@gmail.com>
2024-10-22 21:50:29 +0900

    Updates

--- _nuxt/static/1729601169/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Fgh-blog\u002F_nuxt\u002Fstatic\u002F1729601169",layout:"default",error:a,serverRendered:b,routePath:"\u002F",config:{_app:{basePath:"\u002Fgh-blog\u002F",assetsPath:"\u002Fgh-blog\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
